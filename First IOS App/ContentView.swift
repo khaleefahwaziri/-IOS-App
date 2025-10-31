2cb833fc49b7bb129d1ea4d8409fb8b6dd60be04
khaleefahwaziri <khaleefahabubakar39@gmail.com>
2025-10-31 05:11:00 +0000

    narrow changes that willl be reversed

--- First IOS App/ContentView.swift
+++ First IOS App/ContentView.swift
@@ -1,110 +1,91 @@
-
 import SwiftUI
 
 struct ContentView: View {
     
+    @State private var isNightMoodOn = false
+    
     var body: some View {
+        
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .lightBlue]),
-                           startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(gradient: Gradient(colors: [.blue, .lightBlue]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
             
-            VStack (spacing : 20 ) {
-                Text("Abuja, FCT")
-                    .font(.system(size: 40, weight: .medium))
-                    .foregroundColor(.white)
+            VStack {
                 
-                Image(systemName: "sun.snow.fill")
-                    .renderingMode(.original)
-                    .resizable()
-                    .frame(width: 170, height: 170)
-                Text("24°C")
-                    .font(.system(size: 60, weight: .medium))
-                    .foregroundColor(.white)
+                mainWeatherView(cityName: "Abuja, FCT", imageName: "sun.max.fill", temperature: 33)
                 
-                HStack  {
-                    VStack{
-                        Text("TUE")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                        Text("28°C")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                    }
-                    
-                    VStack{
-                        Text("TUE")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                        Text("28°C")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                    }
-                    
-                    VStack{
-                        Text("TUE")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                        Text("28°C")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                    }
-                    
-                    VStack{
-                        Text("TUE")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                        Text("28°C")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                    }
-                    
-                    VStack{
-                        Text("TUE")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                        Image(systemName: "cloud.sun.fill")
-                            .resizable()
-                            .renderingMode(.original)
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                        Text("28°C")
-                            .font(.system(size: 20, weight: .medium))
-                            .foregroundColor(.white)
-                    }
-                }
-                
-                    
             }
             
             
+            HStack {
+                
+                weekDayView(weekday: "TUE", imageDay: "sunrise.circle.fill", temperatureDay: 5)
+                weekDayView(weekday: "TUE", imageDay: "moon.stars.fill", temperatureDay: 5)
+                weekDayView(weekday: "TUE", imageDay: "sun.max.circle.fill", temperatureDay: 5)
+                weekDayView(weekday: "TUE", imageDay: "moon.fill", temperatureDay: 5)
+                weekDayView(weekday: "TUE", imageDay: "sun.snow.fill", temperatureDay: 5)
+                
+            }
+            
         }
-        .edgesIgnoringSafeArea(.all)
-        
         
     }
-    
 }
+
 
 #Preview {
     ContentView()
+}
+
+
+struct mainWeatherView: View {
+    
+    var cityName: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        
+        VStack (spacing: 8){
+            
+            Text(cityName)
+                .font(.system(size: 40 , weight: .medium , design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 180, height: 180)
+                
+                
+            
+            Text("\(temperature)°C ")
+                .font(.system(size: 70 , weight: .medium , design: .default))
+                .foregroundColor(.white)
+        }
+        .padding(.bottom, 8)
+    }
+}
+
+struct weekDayView: View {
+    
+    var weekday: String
+    var imageDay: String
+    var temperatureDay: Int
+    
+    var body: some View {
+        VStack {
+            Text(weekday)
+                .font(.system(size: 15 , weight: .medium , design: .default))
+                .foregroundColor(.white)
+            
+            Image(systemName: imageDay)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 20, height: 20)
+        }
+    }
+    
 }
