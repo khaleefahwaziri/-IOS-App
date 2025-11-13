83cbe99879732ea65f8923a25d6df66b84f42d14
khaleefahwaziri <khaleefahabubakar39@gmail.com>
2025-11-13 13:42:07 +0000

    casual commit

--- First IOS App/ContentView.swift
+++ First IOS App/ContentView.swift
@@ -107,15 +107,7 @@ struct WeatherDayView: View {
 }
 
 
-struct BackgroundView: View {
-    
-    @Binding var isNightModeOn: Bool
-    
-    var body: some View {
-        LinearGradient(gradient: Gradient(colors: [isNightModeOn ? .black : .blue, isNightModeOn ? .gray : .lightBlue ]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .ignoresSafeArea(.all)
-    }
-}
+
 
 struct MainWeatherView: View {
     
